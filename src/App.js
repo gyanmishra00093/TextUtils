71d70b0721b131b6bed905b622ba56ebd01f1a2b
gyanmishra00093 <gyanprakashmishra00093@gmail.com>
2023-11-14 18:09:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,11 @@
 import './App.css';
 import Navbar from './components/navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
 
 
 function App() {
-  const [mode,setMode]=useState('light');//wheather dark mode enable or not
-
-  const [alert,setAlert]=useState(null);
-
-  const showAlert=(message,type)=>{
-    setAlert({
-      msg:message,
-      type:type
-    })
-    setTimeout(()=>{
-      setAlert(null);
-    },2000);
-  }
-  const toggleMode=()=>{
-    if(mode==='light'){
-      setMode('dark');   
-      document.body.style.backgroundColor='#042743';
-      showAlert("Dark Mode Has Been Enabled","success");
-    }
-    else {
-      setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light Mode Has Been Enabled","success");
-    }
-  }
   return (
     <>
-  <Router>
-  <Navbar title="TextUtils"  aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}  />
-  <Alert alert={alert} />
-   <div className="container my-3"> 
-   <Routes>
-          <Route exact path="/about" element={<About/>}>
-          </Route>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter The Text To Analyse Below"  mode={mode}/>}>
-          </Route>
-   </Routes>
-   </div>
-   </Router>
+  <Navbar mode={mode} toggleMode={toggleMode}  />
  </>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
-export default function navbar(props) {
+export default function navbar() {
+
+  const [mode,setMode]=useState('light');
+  
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');   
+      document.body.style.backgroundColor='#042743';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
+
   return (
     <div>
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-  <Link className="navbar-brand" to="/">{props.title}</Link>
+        <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
+ 
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -14,27 +27,19 @@ export default function navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+        <a className="nav-a" href="/">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/about">{props.aboutText}</Link>
+       <li className="nav-item">
+        <a className="nav-a" href="/about">aboutText</a>
       </li>
       
     </ul>
-    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-  <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+    <div className={`form-check form-switch text-${mode==='light'?'dark':'light'}`}>
+  <input className="form-check-input" onClick={toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
   <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
 </div>
   </div>
 </nav>
     </div>
-    
   )
 }
-
-navbar.propTypes={title:PropTypes.string.isRequired,
-                  aboutText:PropTypes.string.isRequired}
-
-navbar.defaultProp={title:'Set Title Here',
-                   aboutText:'About TextUtils'
-                }
